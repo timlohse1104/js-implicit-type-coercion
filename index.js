2342a9b9dd15104be25cf33d4c45f6214b461805
timlohse1104 <tim-lohse@gmx.de>
2022-07-27 11:00:13 +0200

    Translated to English language

--- index.js
+++ index.js
@@ -1,57 +1,58 @@
-import * as fs from 'fs'
-import * as childProcess from 'child_process'
+import * as fs from "fs";
+import * as childProcess from "child_process";
 
-// Erlaubte Zeichen: ({[/>+!-=\]})
-// JS Basistypen: Strings, Numbers, Booleans, Arrays, Objects
-// Basistypen mit einander verschmelzen -> Type Coercion
+// Allowed characters: ({[/>+!-=\]})
+// JS base types: Strings, Numbers, Booleans, Arrays, Objects
+// Merge base types -> Type Coercion
 
-const zahlen = {}
-const buchstaben = {}
-const zeichen = {}
+const numbers = {};
+const alphabet = {};
+const chars = {};
 
-zahlen['0'] = '+[]'
-zahlen['1'] = '+!![]'
+numbers["0"] = "+[]";
+numbers["1"] = "+!![]";
 
-const erzeugeZahl = zahl => {
-    if(zahl === 0) return zahlen['0']
-    return Array.from({length: zahl}, () => zahlen['1']).join(' + ') 
-}
+const generateNumber = (number) => {
+  if (number === 0) return numbers["0"];
+  return Array.from({ length: number }, () => numbers["1"]).join(" + ");
+};
 
-const test1 = !{}
-const test2 = !!{}
-const test3 = +[]
-const test4 = -[]
-const test5 = +{}
-const test6 = -{}
-const test7 = {}+[]
-const test8 = +!!{}
+const test1 = !{};
+const test2 = !!{};
+const test3 = +[];
+const test4 = -[];
+const test5 = +{};
+const test6 = -{};
+const test7 = {} + [];
+const test8 = +!!{};
 
-console.log(test1)
-console.log(test2)
-console.log(test3)
-console.log(test4)
-console.log(test5)
-console.log(test6)
-console.log(test7)
-console.log(test8)
+console.log(test1);
+console.log(test2);
+console.log(test3);
+console.log(test4);
+console.log(test5);
+console.log(test6);
+console.log(test7);
+console.log(test8);
 
-console.log('2'+ -[])
+console.log("2" + -[]);
 
-console.log(buchstaben)
-console.log(zahlen)
-console.log(zahlen[0])
-console.log(zeichen)
-console.log(erzeugeZahl(3))
+console.log(alphabet);
+console.log(numbers);
+console.log(numbers[0]);
+console.log(chars);
+console.log(generateNumber(3));
 
-const output = `console.log(${erzeugeZahl(15)})`
-fs.writeFileSync('output.js', output)
-const outputResponse = childProcess.execSync('node output.js').toString()
+const output = `console.log(${generateNumber(15)})`;
+fs.writeFileSync("output.js", output);
+const outputResponse = childProcess.execSync("node output.js").toString();
+
 console.log(`
-Content - 'output.js'     :
----------------------------
-${output}
-
-Executed - 'node output.js:
----------------------------
-${outputResponse}
-`)
+| Content - 'output.js'     :
+|----------------------------
+| ${output}
+|
+| Executed - 'node output.js:
+|----------------------------
+| ${outputResponse}
+`);
